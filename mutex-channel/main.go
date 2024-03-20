f7b48d15a724c14dc11f6e011bf1a341a6227efe
Adet <adet@Adetias-MacBook-Pro.local>
2024-03-20 10:49:28 +0700

    mutex

--- mutex-channel/main.go
+++ mutex-channel/main.go
@@ -11,15 +11,22 @@ var wg sync.WaitGroup
 func main() {
 	msg = "hello world"
 
+	var mutex sync.Mutex
+
 	wg.Add(2)
-	go updateMessage("hello universe")
-	go updateMessage("hello cosmos")
+	go updateMessage("hello universe", &mutex)
+	go updateMessage("hello cosmos", &mutex)
 	wg.Wait()
 
 	fmt.Println(msg)
 }
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+
+	m.Lock()
+
 	msg = s
+
+	m.Unlock()
 }
